1bb0c97e5ab328794c82b02a4a4363a519e5615b
polpoti28 <paolopoti28@gmail.com>
2025-10-09 10:01:15 +0000

    Task 1 and 2

---
+++ Challenge2/src/chal2_main.cpp
@@ -0,0 +1,51 @@
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+int main() {
+  int n = 9;
+
+  // ---------
+  // TASK 1
+  // ---------
+
+  MatrixXd A_g(n,n);
+  A_g <<  0, 1, 0, 1, 0, 0, 0, 0, 0,
+          1, 0, 1, 0, 0, 0, 0, 0, 0,
+          0, 1, 0, 1, 1, 0, 0, 0, 0,
+          1, 0, 1, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 1, 0, 1, 1,
+          0, 0, 0, 0, 1, 0, 1, 0, 0,
+          0, 0, 0, 0, 0, 1, 0, 1, 1,
+          0, 0, 0, 0, 1, 0, 1, 0, 1,
+          0, 0, 0, 0, 1, 0, 1, 1, 0;
+
+  cout << A_g.norm() << endl;
+
+  // ---------
+  // TASK 2
+  // ---------
+
+  VectorXd v_g(n);
+  for (int i = 0; i < n; i++) {
+    int sum = 0;
+    for (int j = 0; j < n; j++) {
+      sum += A_g(i,j);
+    }
+    v_g[i] = sum;
+  }
+
+  MatrixXd D_g = v_g.asDiagonal();
+  MatrixXd L_g = D_g - A_g;
+  VectorXd x = VectorXd::Ones(n);
+  VectorXd y = L_g * x;
+  cout << y.norm() << endl;
+
+
+}
+
