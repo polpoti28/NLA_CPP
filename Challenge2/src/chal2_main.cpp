d86836974dcb79c07683264a9554116f3273d0ea
FrancPalad <francesco1.paladini@mail.polimi.it>
2025-10-09 17:39:54 +0200

    Task 3 & 4

--- Challenge2/src/chal2_main.cpp
+++ Challenge2/src/chal2_main.cpp
@@ -47,5 +47,44 @@ int main() {
   cout << y.norm() << endl;
 
 
-}
+  
+  // ---------
+  // TASK 3
+  // ---------
 
+  Eigen::EigenSolver<MatrixXd> solver(L_g);
+  Eigen::VectorXd eivals2 = solver.eigenvalues().real();
+  cout << "The eigenvalues of the L_g matrix are:" << endl << solver.eigenvalues() << endl;
+  cout << "The eigenvectors of the L_g matrix are:" << endl << solver.eigenvectors() << endl;
+
+  double  max_eigenvalue = eivals.real().maxCoeff();
+  cout << "The largest eigenvalue is: " << max_eigenvalue << endl;  
+
+  double  min_eigenvalue = eivals.real().minCoeff();
+  cout << "The smallest eigenvalue is: " << min_eigenvalue << endl;
+
+
+
+  // ---------
+  // TASK 4
+  // ---------
+
+  // remove function does not change the size of the vector, it just moves the elements to be removed to the end of the vector
+  // so i will have the 0  at the end of the vector
+
+  Eigen::VectorXd eivals_new(9);
+
+  int j = 0;
+  for (int i = 0; i < eivals2.size() ; ++i){
+    if (eivals2[i] > 1e-13) 
+        eivals_new[j] = eivals2[i];
+        ++j;
+        
+  }
+
+
+  double second_smallest_eigenvalue = eivals_new.minCoeff();
+  cout << "The second smallest eigenvalue is: " << second_smallest_eigenvalue << endl;
+  cout << "The related eigenvector is: " << endl << solver.eigenvectors().col(0) << endl;
+
+}
