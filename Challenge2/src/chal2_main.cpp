985fdcf3694972e91fd6e1cc2ccca2f7be482c8c
polpoti28 <paolopoti28@gmail.com>
2025-10-17 14:55:09 +0000

    Modified inverse script, corrected some tasks

--- Challenge2/src/chal2_main.cpp
+++ Challenge2/src/chal2_main.cpp
@@ -1,6 +1,7 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 #include <iostream>
+#include <fstream>
 #include <unsupported/Eigen/SparseExtra>
 #include "utils.h"
 
@@ -9,6 +10,7 @@ using std::endl;
 using Eigen::MatrixXd;
 using Eigen::SparseMatrix;
 using Eigen::VectorXd;
+
 
 int main() {
   int n = 9;
@@ -58,7 +60,6 @@ int main() {
   Eigen::SelfAdjointEigenSolver<MatrixXd> solver(L_g);
   Eigen::VectorXd eivals = solver.eigenvalues();
   cout << "The eigenvalues of the L_g matrix are:" << endl << eivals << endl;
-  cout << "The eigenvectors of the L_g matrix are:" << endl << solver.eigenvectors() << endl;
 
   double  max_eigenvalue = eivals.maxCoeff();
   cout << "The largest eigenvalue is: " << max_eigenvalue << endl;  
@@ -101,20 +102,20 @@ int main() {
   // ---------
   
   int n_s = A_s.rows();
-  VectorXd v_s(n_s);
+  SparseMatrix<double> D_s(n_s,n_s);
   for (int i = 0; i < n_s; i++) {
-    int sum = 0;
+    double sum = 0;
     for (int j = 0; j < n_s; j++) {
       sum += A_s.coeff(i, j);
     }
-    v_s[i] = sum;
+    D_s.insert(i, i) = sum;
   }
 
-  MatrixXd D_s = v_s.asDiagonal();
-  MatrixXd L_s = D_s - A_s;
+  SparseMatrix<double> L_s = D_s - A_s;
   cout << "Number of non zero elements in L_s: " << L_s.nonZeros() << endl;
   // Checking if L_s is symmetric
-  if((L_s - L_s.transpose()).norm() < 1e-16)
+  SparseMatrix<double> L_sT = SparseMatrix<double>(L_s.transpose());
+  if((L_s - L_sT).norm() < 1e-16)
     cout << "L_s is symmetric" << endl;
   else
     cout << "L_s is not symmetric" << endl;
@@ -128,14 +129,13 @@ int main() {
    * of L_s, export it to a .mtx file and use a LIS
    * solver to compute the largest eigenvalue of L_s */
 
-  L_s(0, 0) += 0.2;
-  if((L_s - L_s.transpose()).norm() < 1e-10) {
+  L_s.coeffRef(0, 0) += 0.2;
+  SparseMatrix L_sT2 = SparseMatrix<double>(L_s.transpose());
+  if((L_s - L_sT2).norm() < 1e-16) {
     cout << "perturbed L_s is symmetric" << endl;
   } else {
     cout << "perturbed L_s is not symmetric" << endl; 
   }
-  SparseMatrix<double> L_s_sparse;
-  L_s_sparse = L_s.sparseView();
   saveMarket(L_s, "L_s.mtx");
   int ret = system("./run_test.sh");
 
@@ -144,14 +144,14 @@ int main() {
   // TASK 8
   // ---------
 
-  int ret2 = system("./run_testshift.sh");
+  //int ret2 = system("./run_testshift.sh");
 
 
   // ---------
   // TASK 9
   // ---------
 
-  int ret3 = system("./run_test_inverse.sh");
+  //int ret3 = system("./run_test_inverse.sh");
 
 
   // ---------
@@ -160,8 +160,7 @@ int main() {
 
   SparseMatrix<double> eigenVecs;
   loadMarket(eigenVecs, "eigvec_i.mtx");
-  VectorXd eigVec = eigenVecs.col(0);
-  cout << eigVec << endl;
+  VectorXd eigVec = eigenVecs.col(1);
 
   int n_p(0), n_n(0);
   std::vector<int> clusterA;
@@ -177,15 +176,16 @@ int main() {
     }
   }
 
+
   cout << "Number of positive entries: " << n_p << endl;
   cout << "Number of negative entries: " << n_n << endl;
 
-
+  
   // ---------
   // TASK 10
   // ---------
 
-  SparseMatrix<int> P(n_s, n_s);
+  SparseMatrix<double> P(n_s, n_s);
   for (int i = 0; i < n_p; i++) {
     P.coeffRef(i, clusterA[i]) = 1;
   }
@@ -195,4 +195,9 @@ int main() {
   
   SparseMatrix<double> A_ord = P * A_s * P.transpose();
 
+  SparseMatrix<double> nonDiagAord = A_ord.topRightCorner(n_p, n_n);
+  cout << nonDiagAord.nonZeros() << endl;
+
+  SparseMatrix<double> nonDiagAs = A_s.topRightCorner(n_p, n_n);
+  cout << nonDiagAs.nonZeros() << endl;
 }
--- Challenge2/src/run_test_inverse.sh
+++ Challenge2/src/run_test_inverse.sh
@@ -3,4 +3,4 @@ source /u/sw/etc/bash.bashrc
 module load gcc-glibc
 module load lis
 
-mpirun -n 4 ./etest5 L_s.mtx eigval_i.mtx eigvec_i.mtx hist_i.txt iters.txt -e ai -ss 2 -etol 1.e-10 -emaxiter 3000
+mpirun -n 4 ./etest5 L_s.mtx eigval_i.mtx eigvec_i.mtx hist_i.txt iters.txt -e si -ss 2 -etol 1.e-10 -emaxiter 3000
